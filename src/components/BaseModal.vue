e59726a1c078bd3db40709154598995c4296136e
Leon Xu <dcwinfield362@gmail.com>
2022-02-22 16:17:55 -0800

    feat: add media query for the modal-wrap class

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -84,6 +84,9 @@ export default {
     background-color: #31363d;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
       0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    @media (min-width: 400px) {
+      margin-left: -300px;
+    }
 
     input {
       color: #fff;
